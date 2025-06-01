41e1a8aa40c09c0f1d0c7a725ea5c1c3336bfafe
Shulamit-Marmerush <sm6736243@gmail.com>
2025-06-01 06:28:31 +0300

    Update PhotoGallery.tsx
    update gallery

--- client-react/project/src/components/PhotoGallery.tsx
+++ client-react/project/src/components/PhotoGallery.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import * as React from "react"
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
