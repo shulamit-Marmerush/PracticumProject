8c56f5f29eb83236228a037a9d15005b199edbf9
Shulamit-Marmerush <sm6736243@gmail.com>
2025-06-01 06:39:54 +0300

    Update UploadFile.tsx
    update

--- client-react/project/src/components/UploadFile.tsx
+++ client-react/project/src/components/UploadFile.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import type React from "react"
+// import type React from "react"
 import * as React from "react"
 import { useState, useEffect, type ChangeEvent } from "react"
 import axios from "axios"
