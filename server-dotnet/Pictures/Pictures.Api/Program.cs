0f5373bb25bb823ca368a58cd6bc1340e2d9a956
Shulamit-Marmerush <sm6736243@gmail.com>
2025-06-03 06:38:45 +0300

    Update Program.cs

--- server-dotnet/Pictures/Pictures.Api/Program.cs
+++ server-dotnet/Pictures/Pictures.Api/Program.cs
@@ -1,78 +1,31 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Amazon.S3;
-using Amazon.Runtime;
-using Amazon;
-using Pictures.Api;
-using Pictures.Core.Services;
-using Pictures.Service;
-using Pictures.Core.Repositories;
-using Pictures.Data.Repositories;
-using Pictures.Data;
-using Pictures.Core;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;Add commentMore actions
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Configuration.AddJsonFile("secret.json", optional: true, reloadOnChange: true);
 
 
-// רישום שירותי AWS
+//  AWS
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
 builder.Services.AddAWSService<IAmazonS3>();
 
-// הוספת HttpClientFactory
+// äåñôú HttpClientFactory
 builder.Services.AddHttpClient("AIService", client =>
 {
     client.Timeout = TimeSpan.FromSeconds(30);
-});
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pictures API", Version = "v1" });
-    c.OperationFilter<FileUploadOperationFilter>();
-    c.CustomSchemaIds(type => type.FullName);
-    c.DescribeAllParametersInCamelCase();
-    c.IgnoreObsoleteActions();
-    c.IgnoreObsoleteProperties();
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    if (File.Exists(xmlPath))
-    {
-        c.IncludeXmlComments(xmlPath);
-    }
-});
-
-builder.Services.AddCors(opt => opt.AddPolicy("MyPolicy", policy =>
-{
     policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
 }));
 
-// רישום השירותים
+
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<AuthService>();
-builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
-builder.Services.AddScoped<IAlbumRepository, AlbumRepository>();
-builder.Services.AddScoped<IAlbumService, AlbumService>();
-builder.Services.AddScoped<IPhotoService, PhotoService>();
-builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
-builder.Services.AddScoped<IAIService, AIService>();
-builder.Services.AddScoped<IImageProcessingService, ImageProcessingService>();
-builder.Services.AddScoped<ICollageService, CollageService>();
-builder.Services.AddScoped<IEmailService, EmailService>();
+
 
 builder.Services.AddDbContext<DataContext>();
 
-// רישום AutoMapper
+// øéùåí AutoMapper
 builder.Services.AddAutoMapper(cfg =>
 {
     cfg.AddProfile<MappingProfile>();
@@ -80,77 +33,40 @@ builder.Services.AddAutoMapper(cfg =>
 });
 builder.Services.AddSingleton<S3Service>();
 
-// רישום S3 Client
+// øéùåí S3 Client
 builder.Services.AddSingleton<IAmazonS3>(sp =>
 {
     var configuration = sp.GetRequiredService<IConfiguration>();
-    var credentials = new BasicAWSCredentials(
-        configuration["AWS:AccessKey"],
-        configuration["AWS:SecretKey"]
-    );
-    var clientConfig = new AmazonS3Config
-    {
-        RegionEndpoint = RegionEndpoint.GetBySystemName(configuration["AWS:Region"])
-    };
+
     return new AmazonS3Client(credentials, clientConfig);
 });
 
-// רישום OpenRouterService
+//OpenRouterService
 builder.Services.AddScoped<IOpenRouterService>(sp =>
 {
     var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient("AIService");
-    var configuration = sp.GetRequiredService<IConfiguration>();
-    return new OpenRouterService(httpClient, configuration);
-});
 
-builder.Services.AddScoped<IChatService>(sp =>
-{
-    var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient("AIService");
-    var configuration = sp.GetRequiredService<IConfiguration>();
     return new ChatService(httpClient, configuration);
 });
 
-// הוספת JWT Authentication
+// JWT Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
     options.AddPolicy("EditorOrAdmin", policy => policy.RequireRole("Editor", "Admin"));
     options.AddPolicy("ViewerOnly", policy => policy.RequireRole("Viewer"));
 });
-builder.Services.AddApplicationInsightsTelemetry();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-
+}
 
 app.UseHttpsRedirection();
 app.UseCors("MyPolicy");
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
