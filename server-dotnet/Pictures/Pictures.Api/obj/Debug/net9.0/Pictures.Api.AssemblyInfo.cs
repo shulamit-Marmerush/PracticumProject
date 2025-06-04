8cdc94bb9f5fcdc864174c1b3803961eed8bbb9f
Shulamit-Marmerush <sm6736243@gmail.com>
2025-06-04 14:00:26 +0300

    fix program.cs

--- server-dotnet/Pictures/Pictures.Api/obj/Debug/net9.0/Pictures.Api.AssemblyInfo.cs
+++ server-dotnet/Pictures/Pictures.Api/obj/Debug/net9.0/Pictures.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictures.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7689dcc571b338b1f17be1170b2b2fe519f7108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5f8cdc6bfc2aae9dc9ac34137d294dfc2efda7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictures.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictures.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
